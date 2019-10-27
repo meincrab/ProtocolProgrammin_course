1697f384073698447759b56ed5d1c8fdeccb0397
meincrab <meincrab@gmail.com>
2019-10-27 17:07:56 +0200

    Task 3.5 Sending Images + Error handling : done

--- K8684_assignment3.5/client.py
+++ K8684_assignment3.5/client.py
@@ -1,3 +1,21 @@
+''' 
+
+[meincrab@meincrabLap K8684_assignment3.5]$ python client.py 
+Message from server: 0 of 32408 Received
+Message from server: 1024 of 32408 Received
+Message from server: 2048 of 32408 Received
+....
+....
+Message from server: 26624 of 32408 Received
+Message from server: 27648 of 32408 Received
+Message from server: 28672 of 32408 Received
+Message from server: 29696 of 32408 Received
+Message from server: 30720 of 32408 Received
+Message from server: 31744 of 32408 Received
+Files Send
+'''
+
+
 import socket
 import sys
 import os
@@ -5,22 +23,39 @@ import os
 BUFFER_SIZE = 1024
 
 def sendFile(socket):
-    filename = 'heikkiTonni.jpg'
+
+    try:
+        filename = (sys.argv[3])
+        file = open(filename,'rb')
+    except (FileNotFoundError, PermissionError):
+        print("Check the file name, maybe something is wrong")
+        exit()
+
     #open file for reading in binary format
-    file = open(filename,'rb')
     msgSize = os.stat(filename).st_size
     dataSent = 0
-    socket.send((filename+"&").encode())
-    socket.send((str(msgSize) + "&").encode())
-    l = file.read(BUFFER_SIZE)
-    while(l):
-        socket.send(l)
+    try:
+        socket.send((filename+"&").encode())
+        socket.send((str(msgSize) + "&").encode())
         l = file.read(BUFFER_SIZE)
-    file.close
+        while(l):
+            socket.send(l)
+            l = file.read(BUFFER_SIZE)
+            print("Message from server: " + str(s.recv(BUFFER_SIZE), "utf-8"))
+        file.close
+    except(ConnectionError):
+        print("Seems like omething went wrong on the server side")
+        exit()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("localhost", 8888))
+
+try:
+    s.connect((sys.argv[1], int(sys.argv[2])))
+except (ConnectionError):
+    print("Something went wrong while estabilishing connection, check your props")
+    exit()
+
 sendFile(s)
 print("Files Send")
-print(s.recv(BUFFER_SIZE))
+
 s.close()
--- K8684_assignment3.5/server.py
+++ K8684_assignment3.5/server.py
@@ -1,3 +1,20 @@
+'''
+[meincrab@meincrabLap K8684_assignment3.5]$ python server.py 
+Received a connection from  ('127.0.0.1', 34330)
+Name of the input: heikkiTonni.jpg 
+Length of the input: 32408 
+1024 of 32408 Received
+2048 of 32408 Received
+3072 of 32408 Received
+...
+...
+...
+29696 of 32408 Received
+30720 of 32408 Received
+31744 of 32408 Received
+32408 of 32408 Received
+Receiving done
+'''
 import socket
 import sys
 import os
@@ -24,41 +41,60 @@ def incomeLength(client):
     return length
 
 def receiveFile(client, name, length, folder):
-    #open file for writing in binary format
-    file = open(os.path.join(folder, name), 'wb')
+
+    '''
+    Just to remember, can be useful in future
+    if not os.path.exists(folder)
+        os.makedirs(folder)
+    '''    
+    if not os.path.exists(folder):
+        print("Seems like folder is missing")
+        client.send(bytes("Server error", "utf-8"))
+        client.close 
+        exit()
+
+    print("Folder exists!")
+    file = open(os.path.join(folder, name), 'wb')      
     received = 0
     l = client.recv(BUFFER_SIZE)
-    while(l):
-        received += len(l)
-        print("receiving")
-        file.write(l)
-        l = client.recv(BUFFER_SIZE)
-        print (" %d of %s Received" % (received, length))
-    print("Receiving done")
-    file.close()
+    client.send(bytes("%d of %s Received" % (received, length), "utf-8"))  
+    try:
+        while(l):
+            received += len(l)
+            file.write(l)
+            l = client.recv(BUFFER_SIZE)
+            print ("%d of %s Received" % (received, length))
+            client.send(bytes("%d of %s Received" % (received, length), "utf-8"))  
+        file.close()
+        print("Receiving done")
+    except Exception as e:
+        print("Something went wrong during data transfer")
+        client.send(bytes("Server error", "utf-8"))
+        client.close 
+        exit()
 
 
 
 def main():
-    imageFolder = "received-files"
+    imageFolder = (sys.argv[3])
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     #Allows not to wait and avoid "Port already in use " error 
-    sock.bind(("localhost", 8888))
+    sock.bind((sys.argv[1], int(sys.argv[2])))
     sock.listen(5)
-    (client, addr) = sock.accept()
-    dataRec = 0
-    data = ""
-    print("Received a connection from ", addr)
-    nameOf = fileName(client)
-    print("Name of the input: %s " % (nameOf))
-    lengthOf = incomeLength(client)
-    print("Length of the input: %s " % (lengthOf))
-    receiveFile(client, nameOf, lengthOf, imageFolder)
-
     
-
-
+    while True:
+        (client, addr) = sock.accept()
+        dataRec = 0
+        data = ""
+        print("Received a connection from ", addr)
+        nameOf = fileName(client)
+        print("Name of the input: %s " % (nameOf))
+        lengthOf = incomeLength(client)
+        print("Length of the input: %s " % (lengthOf))
+        receiveFile(client, nameOf, lengthOf, imageFolder)
+    
+    
 
 if __name__ == "__main__":
     main()
