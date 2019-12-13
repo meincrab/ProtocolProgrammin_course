1f4ab6af6acc00d6599a1d5f748e2a2be9a3244b
Meincrab <meincrab@gmail.com>
2019-12-13 22:12:36 +0200

    Work In progress

--- K8684_assignment05/client.py
+++ K8684_assignment05/client.py
@@ -1,1 +1,20 @@
-test
+import socket
+import sys
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+try:
+    s.connect((sys.argv[1], int(sys.argv[2])))
+except Exception as e:
+    print(e)
+    print("Something is wrong with arguments!")
+    sys.exit()
+try: 
+    s.send(bytes("hello world", "utf-8"))
+except Exception as e:
+    print("Something went wrong during sending Data")
+    sys.exit()
+print(str(s.recv(1024), "utf-8"))
+s.close()
